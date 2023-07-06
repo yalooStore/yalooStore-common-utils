960aa721703cd36bc49c94093f2b420a8fce5c59
hwiju yeom <yeom456@gmail.com>
2023-07-06 11:13:04 +0900

    Feat:에러메시지 변경을 위한 필드 추가 작업

--- src/main/java/com/yalooStore/common_utils/exception/ClientException.java
+++ src/main/java/com/yalooStore/common_utils/exception/ClientException.java
@@ -8,19 +8,23 @@ import org.springframework.http.HttpStatus;
 /**
  * 클라이언트(사용자, 브라우저)의 요청에 예외가 발생할 경우 사용하는 예외 클래스입니다.
  * */
+@Getter
 public class ClientException extends RuntimeException{
 
     private final ErrorCode errorCode;
     private final HttpStatus responseStatus;
+    private final String displayErrorMessage;
+
 
     public ClientException(ErrorCode errorCode,String message) {
+        this(errorCode, errorCode.getName(), message);
+    }
+
+    public ClientException(ErrorCode errorCode,String message, String displayErrorMessage) {
         super(message);
         this.errorCode = errorCode;
         this.responseStatus = errorCode.getResponseStatus();
-    }
-
-    public String getErrorMessage(){
-        return errorCode.getName();
+        this.displayErrorMessage = displayErrorMessage;
     }
 
 }
--- src/main/java/com/yalooStore/common_utils/exception/ServerException.java
+++ src/main/java/com/yalooStore/common_utils/exception/ServerException.java
@@ -1,21 +1,28 @@
 package com.yalooStore.common_utils.exception;
 
 import com.yalooStore.common_utils.code.ErrorCode;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+
+@Getter
 public class ServerException extends RuntimeException{
 
     private final ErrorCode errorCode;
     private final HttpStatus responseStatus;
+    private final String displayErrorMessage;
+
 
     public ServerException(ErrorCode errorCode, String message) {
+        this(errorCode, errorCode.getName(), message);
+    }
+    public ServerException(ErrorCode errorCode, String message,
+                           String displayErrorMessage) {
         super(message);
         this.errorCode = errorCode;
         this.responseStatus = errorCode.getResponseStatus();
-    }
+        this.displayErrorMessage = displayErrorMessage;
 
-    public String getErrorMessage(){
-        return errorCode.getName();
     }
 
 }
